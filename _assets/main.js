2b6ca23a594366a4fd62cd3cf79be4fc3af84ada
Gaspare Sganga <mail@gasparesganga.com>
2017-04-01 16:03:13 +0200

    .

--- _assets/main.js
+++ _assets/main.js
@@ -1,6 +1,6 @@
-//= require node_modules/jquery/dist/jquery.min.js
+//= require _node_modules/jquery/dist/jquery.min.js
 
-//= require node_modules/swiper/dist/js/swiper.jquery.min.js
+//= require _node_modules/swiper/dist/js/swiper.jquery.min.js
 
 //= require _js/backtotop.jquery.js
 //= require _js/menu.jquery.js
