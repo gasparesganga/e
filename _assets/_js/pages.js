9917d761b69ba2c7c70f9672116750a664534094
Gaspare Sganga <mail@gasparesganga.com>
2017-04-02 21:26:31 +0200

    .

--- _assets/_js/pages.js
+++ _assets/_js/pages.js
@@ -1,11 +1,12 @@
 $(function(){
-    /*
-        Azienda
-    */
-    var swiper = new Swiper(".swiper-container", {
-        pagination              : ".swiper-pagination",
-        nextButton              : ".swiper-button-next",
-        prevButton              : ".swiper-button-prev",
+    
+    /****************************** Azienda ******************************/
+    
+    // Swiper Top
+    var aziendaSwiperTop = new Swiper("#azienda_top_swiper", {
+        pagination              : "#azienda_top_swiper .swiper-pagination",
+        nextButton              : "#azienda_top_swiper .swiper-button-next",
+        prevButton              : "#azienda_top_swiper .swiper-button-prev",
         paginationClickable     : true,
         
         grabCursor              : true,
@@ -19,13 +20,52 @@ $(function(){
         centeredSlides          : true,
         spaceBetween            : 0
     });
-    
     $(window).on("resize", SetSlideWidth);
     SetSlideWidth();
     function SetSlideWidth(){
         var maxWidth = (100 - ($(window).width() * 0.028));
-        $(".swiper-slide").css("max-width", maxWidth + "%");
+        $("#azienda_top_swiper").find(".swiper-slide").css("max-width", maxWidth + "%");
     }
+    
+    
+    // Swiper Partners
+    var aziendaSwiperPartners = new Swiper("#azienda_partners_swiper", {
+        nextButton              : "#azienda_partners_swiper .swiper-button-next",
+        prevButton              : "#azienda_partners_swiper .swiper-button-prev",
+        
+        freeMode                : true,
+        freeModeMomentumRatio   : 0.7,
+        
+        loop                    : true,
+        slidesPerView           : 5,
+        spaceBetween            : 80,
+        loopedSlides            : 5,
+        centeredSlides          : true,
+        
+        zoom                    : true,
+        zoomToggle              : true,
+        zoomMax                 : 5,
+        zoomMin                 : 1,
+        
+        breakpoints             : {
+            1400 : {
+              slidesPerView : 4,
+              spaceBetween  : 70
+            },
+            1000 : {
+              slidesPerView : 3,
+              spaceBetween  : 50
+            },
+            600 : {
+              slidesPerView : 2,
+              spaceBetween  : 30
+            },
+            400 : {
+              slidesPerView : 1,
+              spaceBetween  : 10
+            }
+        }
+    });
     
     
 });
