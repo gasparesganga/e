7cf111fad50a503c391caf85a501f728bd023284
Gaspare Sganga <mail@gasparesganga.com>
2017-12-03 23:09:57 +0100

    .

--- _assets/_js/page_marchi.js
+++ _assets/_js/page_marchi.js
@@ -8,6 +8,49 @@ $(function(){
         appendDots      : "#marchi_pavimenti_dots_container"
     });
     
+    InitSlick("#marchi_rivestimenti_slick", {
+        prevArrow       : "#marchi_rivestimenti_prev",
+        nextArrow       : "#marchi_rivestimenti_next",
+        appendDots      : "#marchi_rivestimenti_dots_container"
+    });
+    
+    InitSlick("#marchi_sanitari_slick", {
+        prevArrow       : "#marchi_sanitari_prev",
+        nextArrow       : "#marchi_sanitari_next",
+        appendDots      : "#marchi_sanitari_dots_container"
+    });
+    
+    InitSlick("#marchi_vasche_slick", {
+        prevArrow       : "#marchi_vasche_prev",
+        nextArrow       : "#marchi_vasche_next",
+        appendDots      : "#marchi_vasche_dots_container"
+    });
+    
+    InitSlick("#marchi_docce_slick", {
+        prevArrow       : "#marchi_docce_prev",
+        nextArrow       : "#marchi_docce_next",
+        appendDots      : "#marchi_docce_dots_container"
+    });
+    
+    InitSlick("#marchi_rubinetterie_slick", {
+        prevArrow       : "#marchi_rubinetterie_prev",
+        nextArrow       : "#marchi_rubinetterie_next",
+        appendDots      : "#marchi_rubinetterie_dots_container"
+    });
+    
+    InitSlick("#marchi_mobili_slick", {
+        prevArrow       : "#marchi_mobili_prev",
+        nextArrow       : "#marchi_mobili_next",
+        appendDots      : "#marchi_mobili_dots_container"
+    });
+    
+    InitSlick("#marchi_riscaldamento_slick", {
+        prevArrow       : "#marchi_riscaldamento_prev",
+        nextArrow       : "#marchi_riscaldamento_next",
+        appendDots      : "#marchi_riscaldamento_dots_container"
+    });
+    
+
     
     function InitSlick(target, options){
         $(target).slick($.extend(true, {
