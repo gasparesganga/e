c4600f51c4b47d5abbd2486bfefd1fc0d628e0bc
Gaspare Sganga <mail@gasparesganga.com>
2017-12-07 20:15:09 +0100

    .

--- _assets/main.js
+++ _assets/main.js
@@ -19,5 +19,10 @@ $(function(){
         enableThrottle  : true,
         throttle        : 250
     });
+    
+    $(document).on("afterChange", ".slick-slider", function(event){
+        $(window).trigger("resize");
+    });
+    
     $(".email_encoded").html("&#105;&#x6e;&#102;&#111;&#x40;&#x65;&#x64;&#105;&#x6c;&#x66;&#x69;&#x6d;&#x65;&#114;&#x2e;&#105;&#116;");
 });
